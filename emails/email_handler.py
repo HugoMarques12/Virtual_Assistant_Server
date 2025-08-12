fae82646575296573f943818e0028f41beafa78b
HugoMarques12 <marquesvhugo7@gmail.com>
2025-08-12 19:34:50 -0300

    Feat: Criar função de login no emailHandler

---
+++ emails/email_handler.py
@@ -0,0 +1,19 @@
+from . import *
+from imap_tools import MailBox, AND
+
+class EmailHandler:
+    def __init__(self):
+        self.imap = MailBox("imap.gmail.com")
+
+    def login(self):
+        try:
+            self.imap.login(emailAddress, emailPassword)
+            return False
+        except:
+            return True
+
+
+if __name__ == "__main__":
+    email_handler = EmailHandler()
+    if email_handler.login():
+        print("Erro ao fazer login. Verifique suas credenciais.")

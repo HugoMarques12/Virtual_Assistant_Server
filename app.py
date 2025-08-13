255d7936d624b8949b2b29611d7ab977fc821816
HugoMarques12 <marquesvhugo7@gmail.com>
2025-08-13 19:16:20 -0300

    Feat: Criar rota para abrir o navegador em determinado site

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from emails.email_handler import *
+from browser.browser_handler import *
+
 
 class EmailCommands:
     def __init__(self):
@@ -21,7 +23,7 @@ class EmailCommands:
         return {"message": self.emailHandler.getEmails()}
 
 email_commands = EmailCommands()
-
+browser_commands = BrowserHandler()
 app = Flask(__name__)
 
 @app.route('/emails/verify_emails')
@@ -31,6 +33,24 @@ def verifyEmails():
 @app.route('/emails/read_emails')
 def readEmails():
     return jsonify(email_commands.readEmails())
+
+@app.route('/browser/open', methods=['POST'])
+def openBrowser():
+    dados = request.get_json()
+    url = dados.get('site')
+    methods = {
+        'youtube': browser_commands.youtube,
+        'google': browser_commands.google,
+        'github': browser_commands.github,
+        'mangalivre': browser_commands.mangalivre
+    }
+
+    if url == 'email':
+        email_name = dados.get('emailName')
+        return jsonify({"message": browser_commands.email(email_name)})
+
+    return jsonify({"message": f'{methods[url]() if url in methods else "Site não encontrado"}'})
+
 
 if __name__ == "__main__":
     app.run()

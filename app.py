722cd69424236822b1b918134f64279e32502a5e
HugoMarques12 <marquesvhugo7@gmail.com>
2025-08-13 17:21:25 -0300

    Refactor: Refatorar rotas

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, jsonify
 from emails.email_handler import *
 
+class EmailCommands:
+    def __init__(self):
+        self.emailHandler = EmailHandler()
+
+    def verifyEmails(self):
+        return {"message": self.emailHandler.verifyNewEmails()}
+    
+    def readEmails(self):
+        return {"message": self.emailHandler.getEmails()}
+
+email_commands = EmailCommands()
+
 app = Flask(__name__)
 
-class Commands:
-    @app.route('/ler_emails')
-    def readEmails():
-        email_handler = EmailHandler()
-        if email_handler.login():
-            return jsonify({"message": "Erro ao fazer login. Verifique suas credenciais."})
+@app.route('/emails/verify_emails')
+def verifyEmails(): 
+    return jsonify(email_commands.verifyEmails())
 
-        return jsonify({"message": email_handler.verifyNewEmails()})
-
+@app.route('/emails/read_emails')
+def readEmails():
+    return jsonify(email_commands.readEmails())
 
 if __name__ == "__main__":
     app.run()

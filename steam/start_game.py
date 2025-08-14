1bc0422262af489b251a2e1fa2df8047c4aa7926
HugoMarques12 <marquesvhugo7@gmail.com>
2025-08-14 08:11:56 -0300

    Feat: Salvar arquivo json

--- steam/start_game.py
+++ steam/start_game.py
@@ -10,6 +10,10 @@ class SteamAppList:
     def _openJsonFile(self):
         with open('steam/appId.json', 'r') as file:
             return json.load(file)
+        
+    def _saveJsonFile(self):
+        with open('steam/appId.json', 'w') as file:
+            json.dump(self.apps, file, indent=4)
 
 
 if __name__ == "__main__":

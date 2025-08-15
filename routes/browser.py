492ea41b56d3326e440808d35ea441f0c8564ee9
HugoMarques12 <marquesvhugo7@gmail.com>
2025-08-15 14:43:29 -0300

    Feat: Criar arquivo exclusivo para rotas do browser

---
+++ routes/browser.py
@@ -0,0 +1,72 @@
+from flask import Blueprint, jsonify, request
+from browser.browser_handler import *
+
+
+browserBp = Blueprint('browser', __name__, url_prefix='/browser')
+
+class BrowserCommands:
+    def __init__(self):
+        self.browserHandler = BrowserHandler()
+
+    def openBrowser(self, data):
+        url = data.get('site')
+        methods = {
+            'youtube': self.browserHandler.youtube,
+            'google': self.browserHandler.google,
+            'github': self.browserHandler.github,
+            'mangalivre': self.browserHandler.mangalivre,
+            'chatgpt': self.browserHandler.chatgpt,
+        }
+        if url == 'email':
+            emailType = data.get('emailType')
+            if emailType not in ['formal', 'jogos']:
+                return {"message": "O tipo de email deve ser 'formal' ou 'jogos'"}
+            
+            return {"url": self.browserHandler.email(emailType)}
+    
+        if url in methods:
+            return {"url": f'{methods[url]()}'}
+        
+        return {'message': 'Não possuo esse site, favor cadastrar no banco de dados'}
+    
+    def search(self, site, query):
+        return {'url': self.browserHandler.search(query, site)}
+    
+
+browser_commands = BrowserCommands()
+
+
+@browserBp.route('/browser/open', methods=['POST'])
+def openBrowser():
+    data = request.get_json()
+
+    chavesExtras = all(key in ['site', 'emailType'] for key in data.keys())
+    chavesFaltando = all(key in data.keys() for key in ['site', 'emailType'])
+
+    if 'site' not in data:
+        return jsonify({'message': 'você não disse o site.'}), 400
+    
+    elif not chavesExtras and chavesFaltando:
+        return jsonify({'message': ''})
+        
+    elif data['site'] == 'email' and any(key not in data for key in ['site', 'emailType']):
+        return jsonify({'message': 'você não incluiu o tipo do email.'}), 400
+        
+    return jsonify(browser_commands.openBrowser(data)), 200
+
+@browserBp.route('/browser/search', methods=['POST'])
+def search():
+    data = request.get_json()
+    query = data.get('query')
+    site = data.get('site')
+
+    if query == None:
+        return jsonify({'message': 'você não incluiu a chave "query" na requisição'}), 400
+    
+    if site == None:
+        return jsonify({'message': 'você não incluiu a chave "site" na requisição'}), 400
+    
+    if site.lower() not in ['google', 'youtube', 'github']:
+        return jsonify({'message': 'o site deve ser "google", "youtube" ou "github"'}), 400
+    
+    return jsonify(browser_commands.search(site.lower(), query.lower())), 200

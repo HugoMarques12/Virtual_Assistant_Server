19143a083654fc6591593134fb4d898eaffe0b62
HugoMarques12 <marquesvhugo7@gmail.com>
2025-08-15 10:03:14 -0300

    Feat: Adicionar verificação de jogos encontrados

--- steam/steam_handler.py
+++ steam/steam_handler.py
@@ -11,16 +11,33 @@ class SteamHandler:
         with open('steam/appId.ini', 'w') as file:
             self.apps.write(file)
     
-    def getAppIds(self, gameNames):
+    def addGame(self, names):
+        foundGames = set()
         response = requests.get(f'{default_endpoint}{apps_endpoint}')
         response = response.json()
 
         for app in response['applist']['apps']:
-            if app['name'].lower() in gameNames:
-                self.apps.set('apps', app['name'].lower().replace(' ', '_'), str(app['appid']))
+            appName = app['name'].lower()
+
+            if appName in names:
+                self.apps.set('apps', appName.replace(' ', '_'), str(app['appid']))
+                foundGames.add(appName)
 
         self._saveFile()
+        return foundGames
+
+    def getAppId(self, gameNames):
+        missingGames = set(name.lower() for name in gameNames)
+        foundGames = self.addGame(gameNames)
+
+        missingGames -= foundGames
+        if missingGames:
+            return list(missingGames)
+        
+        return list(missingGames)
     
     def runGame(self, name):
+        name = name.replace(' ', '_')
         id = self.apps.get('apps', name)
         return f'steam://run/{id}'
+

19bea35c46a8d792113d04238ee88f4052872a71
HugoMarques12 <marquesvhugo7@gmail.com>
2025-08-17 13:34:02 -0300

    Feat: Importar blueprints e registrar rotas

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from routes.browser import browserBp
+from routes.emails import emailsBp
+from routes.steam import steamBp
 
 app = Flask(__name__)
+app.register_blueprint(browserBp)
+app.register_blueprint(emailsBp)
+app.register_blueprint(steamBp)
 
 
 if __name__ == "__main__":
